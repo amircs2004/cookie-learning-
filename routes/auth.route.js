f9f5ffdfa7829b8825cb136c258b4ef2e27c8653
amircs2004 <saidimahdiamir@gmail.com>
2026-07-04 17:34:10 +0100

    adding middleware

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { loginUser, registerUser } = require("../controllers/authCintroller");
 const {hackableUpdate  , testController} = require('../controllers/hackableLogin')
+const protect = require('../middleware/authId')
 router.post("/api/login", loginUser);
 router.post("/api/register", registerUser);
-router.post("/api/hackAbleUpdate", hackableUpdate);
+router.post("/api/hackAbleUpdate", protect, hackableUpdate);
 router.get("/api/testController", testController);
 
 
